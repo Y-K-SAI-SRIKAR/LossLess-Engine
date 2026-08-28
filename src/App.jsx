260fccda1891a35d50f5464f50e6d2dcd0f97265
Y-K-SAI-SRIKAR <saisrikaryerraguntla@gmail.com>
2026-08-29 00:05:27 +0530

    Configured Tailwind and added dependencies and Initiated Visualizations

--- src/App.jsx
+++ src/App.jsx
@@ -220,22 +220,24 @@ function App() {
           </AnimatedContent>
         </div>
       </section>
-      <AnimatedContent
-            distance={100}
-            direction="horizontal"
-            reverse={false}
-            duration={0.9}
-            ease="power3.out"
-            initialOpacity={0}
-            animateOpacity
-            scale={1}
-            threshold={0.1}
-            delay={0}
-          >
+      <div className="Footer-Wrapper">
+        <AnimatedContent
+          distance={100}
+          direction="horizontal"
+          reverse={false}
+          duration={0.9}
+          ease="power3.out"
+          initialOpacity={0}
+          animateOpacity
+          scale={1}
+          threshold={0.1}
+          delay={0}
+        >
           <div className='App-Footer'>
             <span>Defending merchant margins with AI | LossLess Engine | © RazorPay Buildathon 2026</span>
           </div>
         </AnimatedContent>
+      </div>
     </section>
   );
 }
